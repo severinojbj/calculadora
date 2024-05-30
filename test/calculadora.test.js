6062c22ce5ab1ba9f002020846e4b3b9635f57c4
severinojbj <biujose@gmail.com>
2024-05-29 21:04:06 -0300

    commit inicial

---
+++ test/calculadora.test.js
@@ -0,0 +1,9 @@
+const { somar } = require('../src/calculadora');
+const { test, expect } = require ("@jest/globals");
+
+test('Somar dois números', () => {
+  expect(somar(2, 3)).toBe(5);
+  expect(somar(-1, -1)).toBe(-2);
+  expect(somar(2.5, 2.25)).toBe(4.75);
+  expect(somar(10, 0)).toBe(10);
+});
