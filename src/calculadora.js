ee3eb5389a1c04616cfa0dc46f49c309c30e82d3
severinojbj <biujose@gmail.com>
2024-05-29 21:19:02 -0300

    adicionada funcao subtrair

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,6 +1,10 @@
 function somar(a, b) {
+    return a + b;
+}
+
+function subtrair (a, b) {
     return a - b;
 }
 
-module.exports = { somar };
+module.exports = { somar, subtrair };
   
--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,4 +1,4 @@
-const { somar } = require('../src/calculadora');
+const { somar, subtrair } = require('../src/calculadora');
 const { test, expect } = require ("@jest/globals");
 
 test('Somar dois números', () => {
@@ -6,4 +6,11 @@ test('Somar dois números', () => {
   expect(somar(-1, -1)).toBe(-2);
   expect(somar(2.5, 2.25)).toBe(4.75);
   expect(somar(10, 0)).toBe(10);
+});
+
+test('Subtrair dois números', () => {
+  expect(subtrair(2, 3)).toBe(-1);
+  expect(subtrair(-1, -1)).toBe(0);
+  expect(subtrair(2.5, 2.25)).toBe(0.25);
+  expect(subtrair(10, 0)).toBe(10);
 });
